88a504862ce57f2b34083e0662aa426133f57d0a
SWinxy <swinxythecat@gmail.com>
2020-12-06 12:17:24 -0500

    Day 6

---
+++ src/main/java/me/swinxy/aoc2020/Day6.java
@@ -0,0 +1,61 @@
+package me.swinxy.aoc2020;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Day6 {
+
+	public static void main(String[] args) throws IOException {
+		InputStream stream = Day6.class.getClassLoader().getResourceAsStream("day6.txt");
+		if (stream == null) {
+			throw new NullPointerException();
+		}
+
+		String[] groups = new String(stream.readAllBytes()).split("\r\n\r\n");
+
+		int sum = 0;
+
+		for (String group : groups) {
+			Set<Character> uniqueChars = new HashSet<>();
+			for (char c : group.toCharArray()) {
+				if (c >= 'a' && c <= 'z') {
+					uniqueChars.add(c);
+				}
+			}
+			sum += uniqueChars.size();
+		}
+
+		System.out.println(sum);
+
+		sum = 0;
+
+		for (String group : groups) {
+			String[] individuals = group.split("\r\n");
+
+			for (char c = 'a'; c <= 'z'; c++) {
+				boolean allHave = true;
+				for (int i = 0; i < individuals.length && allHave; i++) {
+					allHave = containsChar(individuals[i], c);
+				}
+
+				if (allHave) {
+					sum++;
+				}
+			}
+		}
+
+		System.out.println(sum);
+	}
+
+	private static boolean containsChar(String s, char c) {
+		for (char f : s.toCharArray()) {
+			if (f == c) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
